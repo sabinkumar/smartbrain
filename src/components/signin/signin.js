7f2272bacdf7bd24e092d5b385fd9a26707962e9
sabinyadav <sabin_sky1@hotmail.com>
2019-04-01 06:34:40 +0545

    Updating

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
 
 	onSubmitSignIn = () => {
 		//console.log(this.state);
-		fetch('http://localhost:3001/signin', {
+		fetch('https://mysterious-refuge-57282.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
